0bddd966e82261fc2f0255df4e626d94a1a9a9cf
itsuki0927 <2309899048@qq.com>
2021-01-28 10:43:44 +0800

    完成寻找数组的中心索引

---
+++ js/寻找数组的中心索引.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+  const total = nums.reduce((a, b) => a + b, 0)
+  let sum = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (2 * sum + nums[i] === total) {
+      return i
+    }
+    sum += nums[i]
+  }
+  return -1
+}
