74103f7400fda1b140066e2a5992dc1618a71f9f
itsuki0927 <2309899048@qq.com>
2021-03-28 10:53:37 +0800

    chore(linked-list): improve deleteNode sloving method

--- linked-list/deleteNode.js
+++ linked-list/deleteNode.js
@@ -1,12 +1,5 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
- * 解题思路: 使用双指针
+ * 解题思路: 使用双指针, 找到要删除元素的前一个位置, 然后删除(prev.next -> current.next)
  *
  * @param {ListNode} head
  * @param {number} val
@@ -21,5 +14,29 @@ var deleteNode = function (head, val) {
     current = current.next
   }
   if (current != null) prev.next = current.next
+  return head
+}
+
+// 解题思路: 添加一个虚拟头节点
+var deleteNode = function (head, val) {
+  const fake = new ListNode(-1)
+  fake.next = head
+  let current = fake
+  while (current.next != null) {
+    if (current.next.val == val) {
+      current.next = current.next.next
+      break
+    } else {
+      current = current.next
+    }
+  }
+  return fake.next
+}
+
+// 使用递归
+var deleteNode = function (head, val) {
+  if (head == null) return null
+  if (head.val == val) return head.next
+  head.next = deleteNode(head.next, val)
   return head
 }
