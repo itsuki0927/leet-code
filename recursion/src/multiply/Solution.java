4c0261d8b6cb7e0534815ebf8bf93d929aaf6789
itsuki0927 <2309899048@qq.com>
2021-04-27 15:04:21 +0800

    chore(recursion): finish multiply code

--- recursion/src/multiply/Solution.java
+++ recursion/src/multiply/Solution.java
@@ -1,8 +1,19 @@
-package multiply;/**
-* @module: mall-
-* @description: 
-* @author: itsuki
-* @create: 2021-04-26 20:33
-**/
+package multiply;
+
+/**
+ * 解题思路: 谁大减谁
+ *
+ * @author: itsuki
+ * @create: 2021-04-26 20:33
+ **/
 public class Solution {
+    public int multiply(int A, int B) {
+        if (A == 0 || B == 0) return 0;
+
+        if (A < B) {
+            return multiply(A - 1, B) + B;
+        }
+
+        return multiply(A, B - 1) + A;
+    }
 }
