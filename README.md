2fc26612fecf026bddcb5a880a1f0d163ba99b2c
itsuki0927 <2309899048@qq.com>
2021-06-17 09:21:27 +0800

    feat(dynamic-programming): finish fib

--- README.md
+++ README.md
@@ -210,3 +210,7 @@ leet-code 刷题进行中...
 9. [getPermutation 排列序列](https://leetcode-cn.com/problems/permutation-sequence/)
 10. [binaryTreePath 二叉树的所有路径](https://leetcode-cn.com/problems/binary-tree-paths/)
 11. [findSubsequences 递增子序列](https://leetcode-cn.com/problems/increasing-subsequences/)
+
+## dynamic-programming(动态规划)
+
+1. [fib 斐波那契数列](https://leetcode-cn.com/problems/fibonacci-number/)
---
+++ dynamic-programming/src/fib/Solution.java
@@ -0,0 +1,22 @@
+package fib;
+
+/**
+ * @author: itsuki
+ * @create: 2021-06-17 09:11
+ **/
+public class Solution {
+    public int fib(int n) {
+        if (n == 0) return 0;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().fib(5));
+    }
+}
